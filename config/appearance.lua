39c4c0bb9af6223000624986d586cd228bff3d59
Alan Zhai <zhaixiuchuan@hotmail.com>
2025-08-30 22:29:23 +0800

    fix the pane missing issue

--- config/appearance.lua
+++ config/appearance.lua
@@ -28,6 +28,7 @@ return {
    enable_scroll_bar = true,
 
    -- tab bar
+   enable_wayland = false,
    enable_tab_bar = true,
    hide_tab_bar_if_only_one_tab = false,
    use_fancy_tab_bar = false,
